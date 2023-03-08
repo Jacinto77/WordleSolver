0958ad146094c5e7a0fca7d4e775ec99e7300edd
Max <max@pop-os.localdomain>
2023-03-08 15:29:26 -0500

    made program work again, sstill needs more testing and refinement. Hate the initial control flow, placed comments explaining how I want it done.

--- main.py
+++ main.py
@@ -75,27 +75,27 @@ def letter_in_pos(letter, position, wordlist):
 
 
 def get_choice():
-    print("1: Letter and position")
-    print("2: Letter")
-    print("3: Exclude letter")
+    # print("1: Letter and position")
+    # print("2: Letter")
+    # print("3: Exclude letter")
 
     return int(input())
 
 
-def get_letter_include(letters):
-    input_letters = input("Input letters to include: \n")
-    for s in input_letters:
-        letters.append(s)
-
-    return input_letters
-
-
-def get_letter_exclude(letters):
-    input_letters = input("Input letters to exclude: \n")
-    for c in input_letters:
-        letters.append(c)
-
-    return input_letters
+# def get_letter_include(letters):
+#     input_letters = input("Input letters to include: \n")
+#     for s in input_letters:
+#         letters.append(s)
+#
+#     return input_letters
+#
+#
+# def get_letter_exclude(letters):
+#     input_letters = input("Input letters to exclude: \n")
+#     # for c in input_letters:
+#     #     letters.append(c)
+#
+#     return input_letters
 
 
 def get_position():
@@ -119,8 +119,19 @@ def give_suggestions(wordlist):
 
 # needs testing and fixing
 def display_letters_positions(letters_in_position):
-    for pos, char in letters_in_position:
-        print(pos, ": ", char, end=" ")
+    if letters_in_position[0] is None:
+        return
+    else:
+        for pos, char in letters_in_position:
+            print(pos, ": ", char, end=" ")
+
+
+def get_letter():
+    return input("Input a letter, then answer the prompts:\n>")
+
+
+def assign_position(positions, position, letters):
+    positions[position - 1] = letters
 
 
 # returns list of 5-letter words
@@ -128,40 +139,83 @@ word_list = five_letter_words(remove_newline(fun.load_words(WORDLIST)))
 
 print(word_list)
 
-letters_in_position = []
-letters = []
-rejected = []
+positions = ['_', '_', '_', '_', '_']
+list_letters = []
+list_rejected = []
 suggestions = []
 
+# still hate this control flow
+# would be better to type in a whole string of letters and parse
+# i.e., strings preceded by a '-' are fed into the omission filter
+# strings of multiple letters are fed into the inclusion filter
+# letters preceded by a number are positional indicators, fed into positional filter
 while True:
+    letters = get_letter()
+    print("Is this letter (1)included or (2)excluded?\n>")
     choice = get_choice()
-    # test this option, currently exits with code 1
-    # may need to rethink how I'm providing the parameters and how
-    # they're being added to the arrays
     if choice == 1:
-        position = get_position()
-        letter = get_letter_include(letters_in_position)
-        word_list = letter_in_pos(
-            letter,
-            position,
-            word_list)
-        letters_in_position = letters_in_position.append((position, letter))
+        print("Is the letter (1)positional or (2)not?\n>")
+        choice = get_choice()
+        if choice == 1:
+            position = get_position()
+            word_list = letter_in_pos(
+                letters,
+                position,
+                word_list)
+            list_letters.append(letters)
+            assign_position(positions, position, letters)
+
+        if choice == 2:
+            word_list = return_only_including_x(letters, word_list)
+            list_letters.append(letters)
 
     if choice == 2:
-        word_list = return_only_including_x(
-            get_letter_include(letters),
-            word_list)
+        word_list = return_only_excluding_x(letters, word_list)
+        list_rejected.append(letters)
 
-    if choice == 3:
-        word_list = return_only_excluding_x(
-            get_letter_exclude(rejected),
-            word_list)
+
+    # choice = get_choice()
+    # # test this option, currently exits with code 1
+    # # may need to rethink how I'm providing the parameters and how
+    # # they're being added to the arrays
+    # letters = get_letter()
+    #
+    # if choice == 1:
+    #     position = get_position()
+    #     letter = get_letter_include(letters_in_position)
+    #     word_list = letter_in_pos(
+    #         letter,
+    #         position,
+    #         word_list)
+    #     letters_in_position = letters_in_position.append((position, letter))
+    #
+    # if choice == 2:
+    #     word_list = return_only_including_x(
+    #         get_letter_include(letters),
+    #         word_list)
+    #
+    # if choice == 3:
+    #     word_list = return_only_excluding_x(
+    #         get_letter_exclude(rejected),
+    #         word_list)
 
     print_words(word_list)
 
     print("Number of words: " + str(len(word_list)))
-    display_letters_positions(letters_in_position)
-    print("Letters:\t", str(letters))
-    print("Rejected:\t", str(rejected))
+    print(positions)
+    print("Letters:\t", str(list_letters))
+    print("Rejected:\t", str(list_rejected))
     give_suggestions(word_list)
     print("-------------------------------------")
+
+
+
+# get letter
+# determine if including or excluding
+# if including, need position?
+# if yes, get position
+# if no, skip
+# if excluding, add to rejected letters array
+# if including with position, add to included with position array as a pair (pos, char)
+# if including without position, add to included array
+#
